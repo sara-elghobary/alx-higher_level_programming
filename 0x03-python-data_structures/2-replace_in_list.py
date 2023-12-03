95461f081d8fd24eacc616bb4e1af0c65f0ebcec
sara-elghobary <soorfour@gmail.com>
2023-12-03 14:08:43 +0200

    h

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    lenn = len(my_list) -1
+    lenn = len(my_list) - 1
     if idx < 0 or idx > lenn:
         return my_list
     else:
--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    lenn = len(my_list) -1
+    lenn = len(my_list) - 1
     new_list = my_list.copy()
     if idx < 0 or idx > lenn:
         return new_list
