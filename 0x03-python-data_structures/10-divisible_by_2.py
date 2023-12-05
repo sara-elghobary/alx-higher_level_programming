7e122abe25347b0ac392ec1e84a0d07750761c00
sara-elghobary <soorfour@gmail.com>
2023-12-05 23:25:35 +0200

    h

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    t = []
+    for i in my_list:
+        if i % 2 == 0:
+            t.append(True)
+        else:
+            t.append(False)
+    return t
