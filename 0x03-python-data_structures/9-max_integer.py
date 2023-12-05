0ec1c47f4a0463c8538d98f2b881454fc4780394
sara-elghobary <soorfour@gmail.com>
2023-12-05 23:15:44 +0200

    h

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+def max_integer(my_list=[]):
+    lenn = len(my_list)
+    max = my_list[0]
+    if lenn == 0:
+        return None
+    else:
+        for i in my_list:
+            num = i
+            if num > max:
+                max = num
+    return max
