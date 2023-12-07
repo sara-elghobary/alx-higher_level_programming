5b8c54bdc97c0da613f61e017c22a29734e27e31
sara-elghobary <soorfour@gmail.com>
2023-12-07 15:00:10 +0200

    h

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    a = my_list.copy()
+    for i in range(len(my_list)):
+        if a[i] == search:
+            a[i] = replace
+    return a
