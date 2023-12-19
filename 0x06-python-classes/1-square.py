c62f52895b0b6df3a0beae02eab8bddf98eb9354
sara-elghobary <soorfour@gmail.com>
2023-12-20 00:06:28 +0200

    h

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This module defines a class Square with a private instance attribute.
+"""
+
+
+class Square:
+    """
+    This class represents a square.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes the square with a given size.
+        """
+        self.__size = size
