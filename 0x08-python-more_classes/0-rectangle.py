826e4f5a8f2f73bad4e4a9fc7d019b38cec62828
sara-elghobary <soorfour@gmail.com>
2024-01-04 14:06:58 +0200

    h

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This module defines an empty class Rectangle.
+"""
+
+
+class Rectangle:
+
+    """
+    This class represents a square.
+    It is currently empty.
+    """
+
+    pass:
