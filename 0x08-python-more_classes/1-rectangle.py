046e16ac5be5e41af03609c2fca73e113bc2530d
sara-elghobary <soorfour@gmail.com>
2024-01-04 14:26:39 +0200

    h

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+This module defines an empty class Rectangle.
+"""
+
+
+class Rectangle:
+
+    """
+    This class represents a Rectangle.
+    It is currently empty.
+    """
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+    
+    @property
+    def width(self):
+        return self._width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self._width = value
+
+    @property
+    def height(self):
+        return self._height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self._height = value
