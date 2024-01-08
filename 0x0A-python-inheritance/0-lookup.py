841f786afe10e664952d9519d91a5eeb17617163
sara-elghobary <soorfour@gmail.com>
2024-01-08 15:16:46 +0200

    h

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
+"""Defines an object attribute lookup function."""
+
+
 def lookup(obj):
-    return [
-        attr
-        for attr in dir(obj)
-        if not callable(getattr(obj, attr)) or callable(getattr(obj, attr))
-    ]
+    """Return a list of an object's available attributes."""
+    return (dir(obj))
