f2858275d4c30d6a4238008165771a61e2e13292
sara-elghobary <soorfour@gmail.com>
2024-01-09 22:28:10 +0200

    h

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import json
+"""
+Contains the to_json_string function
+"""
+
+
+def to_json_string(my_obj):
+    """""returns the JSON representation of an object (string)
+
+"""
+    return json.dumps(my_obj)
