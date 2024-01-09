8e9119a68fdc66a9fdf0a9ab5b84422587c0cf9c
sara-elghobary <soorfour@gmail.com>
2024-01-09 21:58:06 +0200

    h

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,14 +1,6 @@
 #!/usr/bin/python3
+
+
 def read_file(filename=""):
-    """
-    Read the contents of a file and print them to the console.
-
-    Parameters:
-    - filename (str): The name of the file to be read. If not
-    provided, an empty string is used.
-
-    Returns:
-    None
-    """
-    with open(filename, encoding="UTF-8") as myfile:
-        print(myfile.read())
+    with open(filename, "r", encoding="UTF-8") as f:
+        print(f.read(), end="")
