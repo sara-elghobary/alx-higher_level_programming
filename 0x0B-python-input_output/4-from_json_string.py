18cb196ab878fbeb6e49365f4dfc77c28cd3cf34
sara-elghobary <soorfour@gmail.com>
2024-01-09 22:38:05 +0200

    h

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Contains the "from_json_string" fundtion
+"""
+
+import json
+
+
+def from_json_string(my_str):
+    """return an object(Python data structure)represented by JSON string"""
+    return json.loads(my_obj)
