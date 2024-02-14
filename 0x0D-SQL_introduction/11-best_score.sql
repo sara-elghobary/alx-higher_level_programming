cd04c4c16184191e2f4fc68890ad28970ea73e6c
sara-elghobary <soorfour@gmail.com>
2024-02-14 15:16:28 +0200

    h

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,4 +1,3 @@
--- lists all records with a score >= 10 in the table second_table 
---Results should display both the score and the name (in this order)
---Records should be ordered by score (top first)
-SELECT score , name FROM second_table ORDER BY score DESC WHERE score >= 10;
+-- List the score greather than or equal to 10
+-- Execute: cat 11-best_score.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
