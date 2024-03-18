b0f37e68c78abb3fdb13114491de5cd5f541ab5c
sara-elghobary <soorfour@gmail.com>
2024-03-19 01:07:18 +0200

    h

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+// 10-factorial.js
+function factorial (n) {
+  if (n === 0 || isNaN(n)) {
+    return 1;
+  }
+
+  return n * factorial(n - 1);
+}
+
+const arg = parseInt(process.argv[2]);
+
+console.log(factorial(arg));
